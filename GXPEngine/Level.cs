b68819bd11628197c2d036fc54ae344caad91c86
lana <svetlasosnova@gmail.com>
2024-02-27 20:43:16 +0100

    i commented the lines which were causing the problem

--- GXPEngine/Level.cs
+++ GXPEngine/Level.cs
@@ -8,69 +8,67 @@ using GXPEngine;
 
 public class Level : GameObject
 {
-	//ArduinoControls arduinoControls;
+    private Player player;
+    private Gem gem;
+    private RotationReader rotationReader;
+    
+    //these lines you added and after that I can no longer run the build. Without them everything works fine.
+    //private ArduinoControls arduinoControls;
 	
-	private Player player;
-	private Gem gem;
-	private RotationReader rotationReader;
+    private int currentTime;
+    private ArrayList gems;
 	
-	private int currentTime;
-	private ArrayList gems;
+    private int gemDestructionPoint;
+    private int gemSpawnTime;
 	
-	private int gemDestructionPoint;
-	private int gemSpawnTime;
-	
-	private Controller controller;
-	public Level(string mapName)
-	{
-		//
-		//arduinoControls = new ArduinoControls();
-		//AddChild(arduinoControls);
+    private Controller controller;
+    public Level(string mapName)
+    {
+        gemDestructionPoint = 600;
+        gemSpawnTime = 3;
 		
-		//
-		gemDestructionPoint = 600;
-		gemSpawnTime = 3;
+        gems = new ArrayList();
+        currentTime = Time.time;
+        TiledLoader loader = new TiledLoader(mapName);
+        loader.rootObject = this;
+        loader.LoadTileLayers(0);
+        loader.autoInstance = true;
+        loader.LoadObjectGroups();
+        player = FindObjectOfType<Player>();
 		
-		gems = new ArrayList();
-		currentTime = Time.time;
-		TiledLoader loader = new TiledLoader(mapName);
-		loader.rootObject = this;
-		loader.LoadTileLayers(0);
-		loader.autoInstance = true;
-		loader.LoadObjectGroups();
-		player = FindObjectOfType<Player>();
+        controller = new Controller(player);
+        AddChild(controller);
 		
-		controller = new Controller(player);
-		AddChild(controller);
-		
-		rotationReader = new RotationReader(controller);
-		AddChild(rotationReader);
+        rotationReader = new RotationReader(controller);
+        AddChild(rotationReader);
 
-	}
+        //these lines you added and after that I can no longer run the build. Without them everything works fine.
+        //arduinoControls = new ArduinoControls();
+        //AddChild(arduinoControls);
+    }
 
-	public void Update()
-	{
-		//arduinoControls.UseFile(controller);
+    public void Update()
+    {
+        //these lines you added and after that I can no longer run the build. Without them everything works fine.
+        //arduinoControls.UseFile(controller);
 		
-		//Console.WriteLine(gems.Count);
-		if (Time.time - currentTime >= gemSpawnTime*1000)
-		{
-			gem = new Gem(rotationReader);
-			//AddChild(gem);
-			gems.Add(gem);
-			currentTime = Time.time;
-		}
+        if (Time.time - currentTime >= gemSpawnTime*1000)
+        {
+            gem = new Gem(rotationReader);
+            //AddChild(gem);
+            gems.Add(gem);
+            currentTime = Time.time;
+        }
 
-		foreach (Gem gem in gems.ToArray())
-		{
-			AddChild(gem);
-			if (gem.y >= gemDestructionPoint)
-			{
-				gems.Remove(gem);
-				gem.Destroy();
-			}
-		}
-	}
+        foreach (Gem gem in gems.ToArray())
+        {
+            AddChild(gem);
+            if (gem.y >= gemDestructionPoint)
+            {
+                gems.Remove(gem);
+                gem.Destroy();
+            }
+        }
+    }
 	
 }
-
